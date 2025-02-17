2232b0326bf1958d7cad78cd12984eacb9112917
lambord4 <molekyla25@gmail.com>
2025-02-17 22:57:08 +0200

    change

--- Homework5/4.js
+++ Homework5/4.js
@@ -3,7 +3,7 @@ let number = parseInt(prompt(`Enter your number`))
 function isPrime(number) {
     if (number <= 2) return (`Число меньше 2х не может быть простым`); 
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
+    for (let i = 2; i <= number; i++) {
         if (number % i === 0) {
             return (`Нет`); 
         }
